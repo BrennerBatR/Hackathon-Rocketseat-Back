1a94308f700df781059e890b5214e05d67202fff
Brennerbbr <brenner.batista@familiapires.com.br>
2019-11-26 15:42:05 -0300

    producao

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/App", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+mongoose.connect(
+  //"mongodb://localhost:27017/Hackathon_Rocketseat"
+  "mongodb+srv://Brenner:yjsSt9Le1eCvHgxP@cluster0-kdimj.mongodb.net/hackathon_devnaescola?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+  }
+);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
