0a4ff1ff2656316ea6ee09c5d320417f73c093c8
Brennerbbr <brenner.batista@familiapires.com.br>
2019-11-26 14:42:19 -0300

    commenters

--- src/app/controllers/matterController.js
+++ src/app/controllers/matterController.js
@@ -10,7 +10,7 @@ router.use(authMiddleware); //verificara o token primeiro
 
 router.get("/", (req, res) => {
   res.send({
-    user: req.userId //caso precise de mais dados no retorno de authenticação, colocá-las na criação do token
+    user: req.userId
   });
 });
 
@@ -22,11 +22,10 @@ router.post("/", async (req, res) => {
     const matter = await Matter.create({
       title,
       school
-    }); //req.userid é preenchido na verificação do token
+    });
 
     await Promise.all(
       modules.map(async module => {
-        //inserindo as modules no matter (primise all esepra tudo executar pra depois salvar)
         const matterModule = new Module({ ...module, matter: matter._id });
         await matterModule.save();
 
@@ -57,7 +56,7 @@ router.get("/", async (req, res) => {
     const matter = await Matter.findById(matterId).populate([
       "user",
       "modules"
-    ]); //populate coloca os dados do usuario na busca
+    ]);
 
     if (matter === null)
       return res.status(404).send({ error: "Matter not found" });
@@ -78,7 +77,7 @@ router.put("/:matterId", async (req, res) => {
         description
       },
       { new: true }
-    ); //retorna os valores atualizados
+    );
 
     matter.modules = [];
     await Module.remove({ matter: matter._id });
@@ -105,7 +104,7 @@ router.delete("/:matterId", async (req, res) => {
   try {
     var matter = await Matter.findByIdAndRemove(req.params.matterId).populate(
       "user"
-    ); //populate coloca os dados do usuario na busca
+    );
     if (matter === null)
       return res.status(404).send({ error: "Matter not found" });
     return res.send({ success: "Matter deleted" });
