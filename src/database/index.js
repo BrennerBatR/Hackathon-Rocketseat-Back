1770fa5215acf651777a4166461e6e279a90adb1
Brennerbbr <brenner.batista@familiapires.com.br>
2019-11-26 10:57:18 -0300

    starting

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/App' , {useNewUrlParser:true ,  useFindAndModify: false });
+mongoose.connect("mongodb://localhost:27017/App", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(bodyParser.json()); //entende q vou envar em JSON
-app.use(bodyParser.urlencoded({extended:false})); //entende que vou passar parametros via url
+app.use(bodyParser.urlencoded({ extended: false })); //entende que vou passar parametros via url
 
-require('./app/controllers/index.js')(app);
+require("./app/controllers/index.js")(app);
 
-app.listen(3000); //porta
+app.listen(3000, () => {
+  console.log("App is listening on port : 3000");
+}); //porta
